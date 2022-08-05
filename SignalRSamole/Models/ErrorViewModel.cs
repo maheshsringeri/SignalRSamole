90929ab6f74a8f263f1ea7d73b33d04966df2bd2
Mahesh <maheshsringeri@gmail.com>
2022-08-05 19:20:12 +0530

    Add project files.

---
+++ SignalRSamole/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SignalRSamole.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
